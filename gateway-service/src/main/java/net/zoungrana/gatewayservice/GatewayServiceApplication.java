64f4b39eb050e5a0411ed72904438db2ec0d22a7
ZOUNGRANA Charly <charlyzoungrana2004@gmail.com>
2026-09-25 14:15:58 +0000

    Création de Eureka-Discovery-Service et configuration dynamique des routes

--- gateway-service/src/main/java/net/zoungrana/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/net/zoungrana/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,13 @@ package net.zoungrana.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -9,5 +16,14 @@ public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
+
+	//@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route("r1",predicates ->predicates.path("/customers/**").uri("lb://CUSTOMER-SERVICE") )
+				.route("r2",predicates ->predicates.path("/products/**").uri("lb://INVENTORY-SERVICE") )
+				.build();
+	};
+
 
 }
---
+++ gateway-service/src/main/java/net/zoungrana/gatewayservice/config/DynamicRoutesConfig.java
@@ -0,0 +1,17 @@
+package net.zoungrana.gatewayservice.config;
+
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamicRoutesConfig {
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
+    }
+}
